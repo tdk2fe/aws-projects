4c8bc232f6f30af562d87a3c46c6f4e7a9400d9b
tdk2fe <tdkrupinski@gmail.com>
2021-10-10 05:08:02 -0500

    initial commit

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+# output "instance_ami" {
+#   value = aws_instance.ubuntu.ami
+# }
+
+# output "instance_arn" {
+#   value = aws_instance.ubuntu.arn
+# }
+
+output "vpc_cidr" {
+    value = data.aws_vpc.default.cidr_block
+}
