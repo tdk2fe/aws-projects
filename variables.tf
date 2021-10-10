904bd8bdd289b9fbd37c1b9799d39895b31bd6fa
tdk2fe <tdkrupinski@gmail.com>
2021-10-10 15:14:32 -0500

    updated data src for subnets

--- variables.tf
+++ variables.tf
@@ -12,10 +12,3 @@ variable "instance_name" {
   description = "EC2 instance name"
   default     = "Provisioned by Terraform"
 }
-
-locals {
-  private_subnets = toset([ for id in data.aws_subnet_ids.private_subnets.ids : id ])
-  
-
-    private_subnet_1 = local.private_subnets[0]
-}
